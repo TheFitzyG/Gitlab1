995939382375a957f7129d8254351c7f61b18e93
TheFitzyG <35902057+TheFitzyG@users.noreply.github.com>
2018-09-21 16:31:16 +0100

    ggg
    gg

---
+++ GameEnginesLab1Tank/Assets/TankController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+	Rigidbody rb;
+
+	public float speed;
+
+	public Transform SpawnPoint;
+	public GameObject Projectile;
+
+	public float shotSpeed = 100f;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+		Vector2 MovementVec = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+
+		MovementVec.Normalize ();
+		MovementVec *= speed;
+
+		rb.AddRelativeForce (0, 0, MovementVec.y);
+	//rb.AddRelativeTorque(0, MovementVec.x, 0);
+		//rb.MoveRotation(transform.rotation 
+
+		//rb.add
+
+		transform.Rotate (0, MovementVec.x  * Time.fixedDeltaTime, 0);
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+
+			GameObject temp = Instantiate (Projectile);
+
+			temp.transform.position = SpawnPoint.position;
+			temp.transform.rotation = SpawnPoint.rotation;
+
+			temp.GetComponent<Rigidbody> ().AddForce (SpawnPoint.forward * shotSpeed, ForceMode.Impulse);
+
+			Destroy (temp, 5f);
+
+		}
+
+	}
+}
---
+++ GameEnginesLab1Tank/Assets/randomColour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomColour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+		Color newCol = Random.ColorHSV (0, 1f, 0.8f, 1f, 0.7f, 0.7f);
+
+		GetComponent<Renderer> ().material.color = newCol;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
